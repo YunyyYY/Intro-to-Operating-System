0c5aab26f9b561562640a3ddd20edbcf601168a1
yunyy <lyguokelly@gmail.com>
2019-03-03 20:10:45 -0600

    null dereference part

---
+++ p3/user/p3.c
@@ -0,0 +1,14 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+
+    char *a = 0;
+
+    printf(1, "p3: a is %c\n", *a);
+
+    exit();
+}
---
+++ p3/user/spin.c
@@ -0,0 +1,21 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        printf(1, "spin currently does nothing!\n");
+        exit();
+    }
+
+    printf(1, "Let's spin for a while...\n");
+    int a = 0;
+    for (;a < atoi(argv[1]);a++){
+        if (a == 0)
+            printf(1, "first round here...\n");
+    }
+    printf(1, "OMG I survived... \n");
+    exit();
+}
